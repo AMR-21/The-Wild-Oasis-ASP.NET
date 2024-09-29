334b8f26a6bbc6ca0d3d9e5e721bc059430a98e0
AMR-21 <amryasser52001@gmail.com>
2024-09-29 18:10:11 +0300

    publish app settings and use dotnet user-secrets

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+var connectionString = builder.Configuration["DB"];
 
 builder.Services.AddDbContext<TheWildOasisContext>(options =>
 {
